20345fc72d9c612fcba187e13c33c3eb3a13af60
nitinrepo-ai <akashchaudhari12349@gmail.com>
2025-08-07 00:15:01 +0700

    Update Orders.jsx

--- src/Orders.jsx
+++ src/Orders.jsx
@@ -10,7 +10,6 @@ function Orders() {
       padding: '40px 20px',
       fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
       marginTop: '80px', // GAP BELOW NAVBAR
-      marginLeft: "550px",
       Display:"block",
       width:"400px"
     },
