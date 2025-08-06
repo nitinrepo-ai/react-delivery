d7eed3a44e2a82c69fb3cfdd4811e3849ac58fc8
nitinrepo-ai <akashchaudhari12349@gmail.com>
2025-08-06 21:27:15 +0700

    Update Signin.jsx

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -22,7 +22,7 @@ function SignIn() {
     alignItems: 'center',
     background: 'linear-gradient(to right, #f8f9fa)',
     fontFamily: '"Segoe UI", Tahoma, Geneva, Verdana, sans-serif',
-     marginTop:"140px",
+     marginTop:"120px",
      marginLeft: "548px",
   };
 
